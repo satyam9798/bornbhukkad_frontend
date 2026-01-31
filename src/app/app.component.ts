6b6b9e8a6ec1a5c46b9eca9820834704c1ca4c11
satyam9798 <satyamshivam9798@gmail.com>
2026-02-01 00:26:30 +0530

    updated: app comp. to handle alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,30 +3,28 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
-import {HttpClientModule} from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 import { ToastService } from '../app/services/toast.service';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-
+import { OrderAlertComponent } from './components/order-alert/order-alert.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
-     RouterOutlet,
-     LoginComponent,
-     SignUpComponent,
-     HttpClientModule,
-     MatSnackBarModule
-    ],
+    RouterOutlet,
+    LoginComponent,
+    SignUpComponent,
+    HttpClientModule,
+    MatSnackBarModule,
+    OrderAlertComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'bb_frontend';
 
   constructor(private toastService: ToastService) {}
-
-
-
 }
